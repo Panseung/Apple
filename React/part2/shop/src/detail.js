9ee13f318ead855e67ad58590f8f633119794068
unknown <jodie9596@gmali.com>
2022-07-25 11:45:40 +0900

    react part2: lesson10

--- React/part2/shop/src/detail.js
+++ React/part2/shop/src/detail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -7,10 +8,24 @@ const YellowBtn = styled.button`
   padding: 10px;
 `;
 export default function Detail(props) {
+  const [sec, setSec] = useState(true);
   const { id } = useParams();
+  useEffect(() => {
+    setTimeout(() => {
+      setSec(false);
+    }, 2000);
+  });
+
+  const [noNumber, setNoNumber] = useState(false);
   return (
     <div className="container">
-      <YellowBtn></YellowBtn>
+      {sec === true ? (
+        <div>
+          <YellowBtn></YellowBtn>
+          <h3 style={{ cursor: "pointer" }}> 2초 이내 구매시 할인 </h3>
+        </div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
           <img
@@ -26,6 +41,29 @@ export default function Detail(props) {
           <p>{props.shoes[id].price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
+        <h2>숫자만 입력하세요</h2>
+        <input
+          style={{ width: "30%", marginLeft: "auto", marginRight: "auto" }}
+          onChange={(e) => {
+            if (isNaN(Number(e.target.value))) {
+              setNoNumber(true);
+            } else {
+              setNoNumber(false);
+            }
+          }}
+        ></input>
+        {noNumber ? (
+          <h3
+            style={{
+              color: "red",
+              MozUserSelect: "none",
+              MsUserSelect: "none",
+              userSelect: "none",
+            }}
+          >
+            숫자만 입력하세요
+          </h3>
+        ) : null}
       </div>
     </div>
   );
